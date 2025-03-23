d8d87c4f145cdc43499fb44a09ee7cd85605b2db
Engnzlz <engzlz@outlook.com>
2025-03-23 18:25:01 -0500

    Add ngx-owl-carousel-o dependency, enhance KPI and glossary components, and improve styling for various components

--- src/app/modules/glossary/glossary.component.ts
+++ src/app/modules/glossary/glossary.component.ts
@@ -3,12 +3,10 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-glossary',
-  standalone : true,
-  imports: [
-    CommonModule
-  ],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './glossary.component.html',
-  styleUrl: './glossary.component.scss'
+  styleUrls: ['./glossary.component.scss']
 })
 export class GlossaryComponent {
 
--- src/app/modules/kpi/kpi.component.ts
+++ src/app/modules/kpi/kpi.component.ts
@@ -4,12 +4,43 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-kpi',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   templateUrl: './kpi.component.html',
-  styleUrl: './kpi.component.scss'
+  styleUrls: ['./kpi.component.scss']
 })
 export class KpiComponent {
-
+  kpis = [
+    {
+      title: 'Ingresos Mensuales',
+      value: '$50,000',
+      trend: '↑ 20%',
+      trendClass: 'trend-up',
+      period: 'Mes Actual',
+      description: 'Ingresos totales generados en el mes actual.'
+    },
+    {
+      title: 'Tasa de Conversión',
+      value: '2.5%',
+      trend: '↓ 0.5%',
+      trendClass: 'trend-down',
+      period: 'Últimos 7 días',
+      description: 'Porcentaje de visitantes que completaron una acción deseada.'
+    },
+    {
+      title: 'Satisfacción del Cliente',
+      value: '4.8/5',
+      trend: '→',
+      trendClass: 'trend-stable',
+      period: 'Último Trimestre',
+      description: 'Puntuación promedio de satisfacción del cliente.'
+    },
+    {
+      title: 'Usuarios Activos Diarios',
+      value: '12,345',
+      trend: '↑ 5%',
+      trendClass: 'trend-up',
+      period: 'Hoy',
+      description: 'Número de usuarios únicos que interactuaron con la plataforma hoy.'
+    },
+  ];
 }
