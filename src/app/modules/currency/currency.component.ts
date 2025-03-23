0f1fb3304ae302abc8863910f4257aefdf8f417c
Engnzlz <engzlz@outlook.com>
2025-03-23 16:33:21 -0500

    Enhance currency component with new layout and styling, update language colors

--- src/app/modules/currency/currency.component.ts
+++ src/app/modules/currency/currency.component.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-currency',
-  imports: [
-    CommonModule
-  ],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './currency.component.html',
-  styleUrl: './currency.component.scss'
+  styleUrls: ['./currency.component.scss']
 })
 export class CurrencyComponent {
+  coins = [
+    { name: 'Quarter', imageUrl: '../../../assets/images/currency/quarter.png' },
 
+    { name: 'Penny', imageUrl: '../../../assets/images/currency/quarter.png' },
+  ];
+
+  bills = [
+    { name: 'Dollar', imageUrl: '../../../assets/images/currency/dollar.png', value: '$1' },
+    { name: 'Five Dollar', imageUrl: '../../../assets/images/currency/5dollar.png'
+      , value: '$5' }
+  ];
 }
